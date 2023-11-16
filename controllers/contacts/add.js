fd6ef4781e3ac8990f007e1f02dafe79a6c42bfd
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-11-16 13:48:39 +0200

    initial commit

--- controllers/contacts/add.js
+++
@@ -1,8 +0,0 @@
-const Contact = require('../../models/contact');
-
-const add = async (req, res) => {
-  const { _id: owner } = req.user;
-  res.status(201).json(await Contact.create({ ...req.body, owner }));
-};
-
-module.exports = add;
--- controllers/users/getCurrent.js
+++
@@ -1,9 +0,0 @@
-const getCurrent = (req, res) => {
-  const { email, name } = req.user;
-  res.json({
-    name,
-    email,
-  });
-};
-
-module.exports = getCurrent;
