e9b26d171cbef8ce9594285fe712f6dd8e29fa93
elfnesikogorosta <anatoliy.tymoshenko.work@gmail.com>
2023-11-18 22:03:12 +0200

    v1.0

--- routes/api/petRoute.js
+++ routes/api/petRoute.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const { validateBody } = require("../../middlewares");
 const petSchema = require("../../schemas/joiPetsSchema");
+const {
+  petList,
+  addPet,
+  deletePet,
+  petInfo,
+} = require("../../controllers/pets");
 
 const router = express.Router();
 
-router.get("/");
-
+router.get("/", petList);
+router.post("/", validateBody(petSchema), addPet);
+router.get("/:id", petInfo);
+router.delete("/:id", deletePet);
 module.exports = router;
