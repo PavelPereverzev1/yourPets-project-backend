6847a9c0e6a64e8dfcfd3785bfdd073d881c77e1
elfnesikogorosta <anatoliy.tymoshenko.work@gmail.com>
2023-11-18 08:56:05 +0200

    v1.0

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-const express = require('express');
-const logger = require('morgan');
-const cors = require('cors');
-require('dotenv').config();
-const {swaggerRouter,authRouter} = require('./routes');
+const express = require("express");
+const logger = require("morgan");
+const cors = require("cors");
+require("dotenv").config();
+const { swaggerRouter, authRouter } = require("./routes");
+const petRouter = require("./routes/api/petRoute");
 
 // const contactsRouter = require('./routes/api/contacts');
 // const usersRouter = require('./routes/api/users');
 
 const app = express();
 
-const formatLogger = app.get('env') === 'development' ? 'dev' : 'short';
+const formatLogger = app.get("env") === "development" ? "dev" : "short";
 
 app.use(logger(formatLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use('/api-docs',swaggerRouter);
+app.use("/api-docs", swaggerRouter);
 
-app.use('/auth',authRouter);
-
+app.use("/auth", authRouter);
+app.use("/notices", petRouter);
 app.use((req, res) => {
-  res.status(404).json({ message: 'Not found' });
+  res.status(404).json({ message: "Not found" });
 });
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = 'Server error' } = err;
+  const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
