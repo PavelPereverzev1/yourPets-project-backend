e5f4677bc86699576dd7d834059de59a25f9712e
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-11-19 11:20:20 +0200

    get changes

--- helpers/index.js
+++ helpers/index.js
@@ -2,10 +2,12 @@ const HttpError = require('./HttpError');
 const ctrlWrapper = require('./ctrlWrapper');
 const handleMongooseError = require('./handleMongooseError.js');
 const generateJWT = require('./generateJWT.js');
+const upload = require('./upload');
 
 module.exports = {
     HttpError,
     ctrlWrapper,
     handleMongooseError,
     generateJWT,
+    upload,
   };
