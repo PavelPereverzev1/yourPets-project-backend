1ed9250f9559b390e79e66ba904e9855e20f8cb3
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-11-25 23:42:40 +0200

    bug fix

--- controllers/notices/getFavorites.js
+++ controllers/notices/getFavorites.js
@@ -2,15 +2,18 @@ const Notice = require("../../models/notice.js");
 
 const getFavorites = async (req, res) => {
   const { favorites } = req.user;
+  const { page = 1, limit = 12, search = "" } = req.query;
+
   const result = [];
   for (const item of favorites) {
     const notice = await Notice.findById(item);
     result.push(notice);
-    console.log(notice);
   }
-  const { page = 1, limit = 12 } = req.query;
+  const searchResult = result.filter((item) =>
+    item.title.toLowerCase().includes(search.trim().toLowerCase())
+  );
   const skip = (page - 1) * limit;
-  const resultPage = result.slice(skip, skip + limit);
+  const resultPage = searchResult.slice(skip, skip + limit);
 
   res.status(200).json({
     data: resultPage,
--- controllers/notices/getOwnNotices.js
+++ controllers/notices/getOwnNotices.js
@@ -1,15 +1,21 @@
 const Notice = require("../../models/notice.js");
 
 const getOwnNotices = async (req, res) => {
+  const { page = 1, limit = 12, search = "" } = req.query;
   const { _id: owner } = req.user;
-  const { page = 1, limit = 12 } = req.query;
-  const skip = (page - 1) * limit;
-  res.status(200).json(
-    await Notice.find({owner}, "-createdAt -updatedAt", {
-      skip,
-      limit,
-    })
+
+  const result = await Notice.find({ owner }, "-createdAt -updatedAt");
+
+  const searchResult = result.filter((item) =>
+    item.title.toLowerCase().includes(search.trim().toLowerCase())
   );
+  const skip = (page - 1) * limit;
+  const resultPage = searchResult.slice(skip, skip + limit);
+
+  res.status(200).json({
+    data: resultPage,
+    total: result.length,
+  });
 };
 
 module.exports = getOwnNotices;
