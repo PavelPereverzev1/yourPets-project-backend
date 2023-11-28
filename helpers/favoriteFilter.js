1b57fdab49f6e93d2ea92a112b0841265522af6f
Pavel Pereverziev <pereverzev.pavel1975@gmail.com>
2023-11-29 01:27:30 +0200

    fix favorite filter helper

--- helpers/favoriteFilter.js
+++ helpers/favoriteFilter.js
@@ -3,9 +3,14 @@ const dateFns = require("date-fns");
 const favoriteFilter = (prams, noticeArray) => {
   const { page = 1, limit = 12, search = "" } = prams;
 
-  const searchResult = noticeArray.filter((item) =>
-    item.title.toLowerCase().includes(search.trim().toLowerCase())
-  );
+  let searchResult = [];
+  if (search) {
+    searchResult = noticeArray.filter((item) =>
+      item.title.toLowerCase().includes(search.trim().toLowerCase())
+    );
+  } else {
+    searchResult = noticeArray;
+  }
 
   const skip = (page - 1) * limit;
   const resultPage = searchResult.slice(skip, skip + limit);
